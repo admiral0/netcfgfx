7b2c5c36cf564900ddbf0f901bc06e95c5d44932
admiral0 <admiral0@tuxfamily.org>
2010-10-05 21:42:15 +0200

    Fixed notifications

--- src/netcfgfx_notifier.cpp
+++ src/netcfgfx_notifier.cpp
@@ -4,7 +4,7 @@
 netcfgfx_notifier::netcfgfx_notifier(QDBusConnection &conn,QObject *parent) :
     QSystemTrayIcon(parent)
 {
-    notify=new OrgFreedesktopNotificationsInterface("org.freedesktop.Notifications","/",conn,parent);
+    notify=new OrgFreedesktopNotificationsInterface("org.freedesktop.Notifications","/org/freedesktop/Notifications",conn,parent);
 }
 void netcfgfx_notifier::showMessage(const QString &title, const QString &msg, MessageIcon icon, int msecs){
     notify->Notify("netcfgfx",0,"",title,msg,QStringList(),QVariantMap(),msecs);
